9a3fe84670131dae543b450c61a9ef3d26e54a42
F1r3n25 <dimalikegtr@gmail.com>
2023-11-18 23:42:21 +0200

    first

---
+++ seeds.py
@@ -0,0 +1,41 @@
+import json
+from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE, NotUniqueError
+from password import PASS_
+
+host = f"mongodb+srv://dmitriy_test:{PASS_}@homework8.21evbba.mongodb.net/?retryWrites=true&w=majority"
+
+connect(host=host, ssl=True)
+
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = StringField(max_length=50)
+    born_location = StringField(max_length=150)
+    description = StringField()
+    meta = {"collection": "authors"}
+
+
+class Quote(Document):
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=50))
+    quote = StringField()
+    meta = {"collection": "quotes"}
+
+
+if __name__ == '__main__':
+    with open('authors.json', encoding='utf-8') as fd:
+        data = json.load(fd)
+        for el in data:
+            try:
+                author = Author(fullname=el.get('fullname'), born_date=el.get('born_date'),
+                                born_location=el.get('born_location'), description=el.get('description'))
+                author.save()
+            except NotUniqueError:
+                print(f"Автор вже існує {el.get('fullname')}")
+
+    with open('quotes.json', encoding='utf-8') as fd:
+        data = json.load(fd)
+        for el in data:
+            author, *_ = Author.objects(fullname=el.get('author'))
+            quote = Quote(quote=el.get('quote'), tags=el.get('tags'), author=author)
+            quote.save()
